b04cdd9e1b248956396d5d81066b3c2be1ac3e39
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-09-16 15:04:20 -0700

    Completed task 3.

--- index.js
+++ index.js
@@ -127,6 +127,9 @@ class Lambdasian {
     this.age = attributes.age;
     this.location = attributes.location;
   }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
+  }
 }
 
 /*
